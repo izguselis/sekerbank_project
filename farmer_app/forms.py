141047877d04c47ce2f7039cd990599e78a431b2
Merve_Dell <mervetok91@gmail.com>
2021-03-16 14:56:30 +0300

    first commit merv

---
+++ farmer_app/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.db.models import Q
+from django.forms import CharField, NumberInput
+
+from .models import *
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'category',
+            'name_tr',
+            'name_en', 'status', 'price', 'photo'
+        ]
--- farmer_app/models.py
+++ farmer_app/models.py
@@ -1,11 +1,29 @@
 from django.db import models
+import os
 
 
 # Create your models here.
+def upload_form(instace, filename):
+    return os.path.join('uploads/photo/', filename)
+
+
 class Category(models.Model):
-    category_name = models.CharField(max_length=100)
-    status = models.BooleanField(default=False)
+    category_name = models.CharField(max_length=100, verbose_name='Kategori Adı')
+    status = models.BooleanField(default=False, verbose_name='Durum')
+
+    def __str__(self):
+        return self.category_name
 
 
-def __str__(self):
-    return self.category_name
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='p_category', null=True, blank=True)
+    name_tr = models.CharField(max_length=250, null=True, blank=True)
+    name_en = models.CharField(max_length=250, null=True, blank=True)
+    status = models.BooleanField(default=False, verbose_name='Durumu')
+    price = models.DecimalField(blank=True, null=True, verbose_name='Fiyat',
+                                max_digits=7,
+                                decimal_places=2)
+    photo = models.FileField(upload_to=upload_form)
+
+    def __str__(self):
+        return self.name_tr
