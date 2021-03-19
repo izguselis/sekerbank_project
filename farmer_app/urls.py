2502108842e7386a008ff58dc9194ba09b3be0a6
Merve_Dell <mervetok91@gmail.com>
2021-03-19 16:50:01 +0300

    file upload hata çözümü, form'da manuel design

--- farmer_app/urls.py
+++ farmer_app/urls.py
@@ -1,26 +1,27 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from . import views
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.login, name="login"),
-    path('index/', views.index, name="index"),
-    path('register/', views.register, name="register"),
-    path('reset_password/', views.reset_password, name="reset_password"),
+                  path('', views.login, name="login"),
+                  path('index/', views.index, name="index"),
+                  path('register/', views.register, name="register"),
+                  path('reset_password/', views.reset_password, name="reset_password"),
 
-    path('category/', views.category, name="category"),
-    path('add_category/<category_id>', views.add_category,
-         name="add_category"),
-    path('delete_category/<Category_id>', views.delete_category,
-         name="delete_category"),
+                  path('category/', views.category, name="category"),
+                  path('add_category/<category_id>', views.add_category,
+                       name="add_category"),
+                  path('delete_category/<Category_id>', views.delete_category,
+                       name="delete_category"),
 
-    path('product/', views.product, name="product"),
-    path('add_product/<product_id>', views.add_product, name="add_product"),
-    path('delete_product/<product_id>', views.delete_product,
-         name="delete_product"),
+                  path('product/', views.product, name="product"),
+                  path('add_product/<pk>', views.add_product, name="add_product"),
+                  path('delete_product/<product_id>', views.delete_product,
+                       name="delete_product"),
 
-    path('cart/', views.cart, name="cart"),
-    path('add_cart/<product_id>', views.add_cart, name="add_cart"),
-    path('delete_from_cart/<product_id>', views.delete_from_cart,
-         name="delete_from_cart"),
-]
+                  path('cart/', views.cart, name="cart"),
+                  path('add_cart/<product_id>', views.add_cart, name="add_cart"),
+                  path('delete_from_cart/<product_id>', views.delete_from_cart,
+                       name="delete_from_cart"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
