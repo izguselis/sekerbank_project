ba18fbea943b7b43ad3844b9f44205ea872c5b3b
izguselis <selisaydin@gmail.com>
2021-03-23 13:50:59 +0300

    [change] form design

--- farmer_app/models.py
+++ farmer_app/models.py
@@ -8,9 +8,12 @@ def upload_form(instance, filename):
 
 
 class Category(models.Model):
-    category_name = models.CharField(verbose_name='Kategori Adı', max_length=100)
-    status = models.BooleanField(verbose_name='Durumu', default=False)
-    category_image = models.ImageField(verbose_name='Kategori Görseli', upload_to=upload_form, blank=True)
+    category_name = models.CharField(verbose_name='Kategori Adı',
+                                     max_length=100)
+    status = models.BooleanField(verbose_name='Durumu',
+                                 default=False)
+    category_image = models.ImageField(verbose_name='Kategori Görseli',
+                                       upload_to=upload_form, blank=True)
 
     def __str__(self):
         return self.category_name
@@ -18,17 +21,28 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-                                 related_name='p_category', null=True,
+                                 related_name='p_category',
+                                 null=True,
                                  blank=True)
-    name_tr = models.CharField(max_length=250, null=True, blank=True,
+    name_tr = models.CharField(max_length=250,
+                               null=True,
+                               blank=True,
                                verbose_name='Ürün Adı (TR)')
-    name_en = models.CharField(max_length=250, null=True, blank=True,
+    name_en = models.CharField(max_length=250,
+                               null=True,
+                               blank=True,
                                verbose_name='Ürün Adı (EN)')
-    status = models.BooleanField(default=False, verbose_name='Durumu')
-    price = models.DecimalField(blank=True, null=True, verbose_name='Fiyat',
+    status = models.BooleanField(default=False,
+                                 verbose_name='Durumu')
+    price = models.DecimalField(blank=True,
+                                null=True,
+                                verbose_name='Fiyat',
                                 max_digits=7,
                                 decimal_places=2)
-    photo = models.FileField(upload_to=upload_form, null=True, blank=True, max_length=1000,
+    photo = models.FileField(upload_to=upload_form,
+                             null=True,
+                             blank=True,
+                             max_length=1000,
                              verbose_name='Ürün Görseli')
 
     def __str__(self):
@@ -36,9 +50,11 @@ class Product(models.Model):
 
 
 class OrderItem(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.SET_NULL,
+    product = models.OneToOneField(Product,
+                                   on_delete=models.SET_NULL,
                                    null=True)
-    quantity = models.IntegerField(verbose_name='Miktar', default=1)
+    quantity = models.IntegerField(verbose_name='Miktar',
+                                   default=1)
     is_ordered = models.BooleanField(default=False)
     date_ordered = models.DateTimeField(null=True)
 
